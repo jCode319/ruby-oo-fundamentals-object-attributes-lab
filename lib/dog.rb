0fdcd159f523046e726a993dd4b4a384a1683a5e
jCode319 <jdjack3@gmail.com>
2020-10-08 12:51:15 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+    # def initialize(str)
+    #     @name = str
+    # end
+
+    def name=(str)
+        @name = str
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(str)
+        @breed = str
+    end
+
+    def breed
+        @breed
+    end
+
+
+end
+
+
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+    def name=(str)
+        @name = str
+    end
+
+    def name
+        @name
+    end
+
+    def job=(str)
+        @job = str
+    end
+
+    def job
+        @job
+    end
+
+end
